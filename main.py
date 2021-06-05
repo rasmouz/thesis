be9a24fe6219325465dd43e1e2b0bfaa78155b59
Rasmus Roslund <roslund.rasmus@gmail.com>
2021-06-05 10:08:03 +0200

    added print evaluation when logging, and added a .gitignore

--- main.py
+++ main.py
@@ -565,11 +565,15 @@ def train():
 
         if batch % args.log_interval == 0 and batch > 0:
             cur_loss = total_loss / args.log_interval
+            model.eval()
+            val_loss = evaluate(val_data)
+            model.train()
             elapsed = time.time() - start_time
             print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | '
-                  'loss {:5.2f} | ppl {:8.2f}'.format(
+                  ' train ppl {:8.2f} | val ppl {:8.2f}'.format(
                       epoch, batch, len(train_data) // args.bptt, lr,
-                      elapsed * 1000 / args.log_interval, cur_loss, math.exp(cur_loss)))
+                      elapsed * 1000 / args.log_interval, math.exp(cur_loss),
+                      math.exp(cur_loss)))
             total_loss = 0.
             start_time = time.time()
 
