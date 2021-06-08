e18c97661bc2b3a0f84ece7971e1f01611e8bdab
Rasmus Roslund <roslund.rasmus@gmail.com>
2021-06-09 01:43:38 +0200

    Added functionality: an argument parser, a monolingual option and a plot option.

--- grammaticality.py
+++ grammaticality.py
@@ -1,11 +1,22 @@
 import sys
 from scipy import stats
-folder = sys.argv[1]
+import argparse
+import matplotlib.pyplot as plt
+
+parser = argparse.ArgumentParser(description='Computes the mean surprisal for V3 and Det1 and the p-value of a paired t-test')
+parser.add_argument('--language', type=str, default=None,
+                    choices=['english', 'dutch'],
+                    help='If tested on one language only, specify the language')
+parser.add_argument('--data_dir', type=str, default='./',
+                    help='location of the surprisal data')
+parser.add_argument('--plot', action='store_true',
+                    help='plot the results')
+args = parser.parse_args()
 
 def mean_surprisal(file, indices):
     index=0
-    surs = []
-    surs_det = []
+    surs_v3 = dict()
+    surs_det1 = dict()
     with open(file) as techno:
         for line in techno:
             split = line.split()
@@ -14,27 +25,21 @@ def mean_surprisal(file, indices):
             searched_word_index = indices[index]
             if split[2] == str(indices[index-1]+1 + (index==5)) and int(split[1])==index-1:
                 if not unk:
-                    #print('det',split[1], split[0], split[4], 'yes')
-                    surs_det.append(split[4])
-                else:
-                    #print('det',split[1], split[0], split[4], 'no')
-                    pass
+                    surs_det1[int(split[1])+1] = float(split[4])
             if split[2] == str(indices[index]) and int(split[1])==index:
                 if split[0]!='<unk>':
                     unk = False
-                    #print(split[1], split[0], split[4], 'yes')
-                    surs.append(split[4])
+                    surs_v3[int(split[1])+1] = float(split[4])
                 else:
                     unk = True
-                    #print(split[1], split[0], split[4], 'no')
                 index += 1
 
             if index > 16:
                 break
-    return sum([float(sur) for sur in surs])/len(surs), \
-            sum([float(sur) for sur in surs_det])/len(surs_det), \
-            [float(sur) for sur in surs], \
-            [float(sur) for sur in surs_det]
+    return sum(list(surs_v3.values()))/len(surs_v3), \
+            sum(list(surs_det1.values()))/len(surs_det1), \
+            surs_v3, \
+            surs_det1
 
 english_indices = \
 {
@@ -63,7 +68,7 @@ dutch_indices = \
     1:12, #unk
     2:12, #unk
     3:12, #unk
-    4:14, #also 15
+    4:14, #unk
     5:12,
     6:12, #unk
     7:12,
@@ -78,14 +83,37 @@ dutch_indices = \
     16:0
 }
 
-eg = mean_surprisal(folder+'english_gram_result.txt', [i for i in list(english_indices.values())])
-eu = mean_surprisal(folder+'english_ungram_result.txt', [i-1 for i in list(english_indices.values())])
-dg = mean_surprisal(folder+'dutch_gram_result.txt', [i for i in list(dutch_indices.values())])
-du = mean_surprisal(folder+'dutch_ungram_result.txt', [i-1 for i in list(dutch_indices.values())])
-print("english grammatical:  V3 {} Det1 {}".format(eg[0], eg[1]))
-print("english ungrammatical: V3 {} Det1 {}".format(eu[0], eu[1]))
-print("english p-values: V3 {} Det1 {}".format(stats.ttest_rel(eg[2],eu[2])[1], stats.ttest_rel(eg[3],eu[3])[1]))
-print('-'*65)
-print("dutch grammatical:  V3 {} Det1 {}".format(dg[0], dg[1]))
-print("dutch ungrammatical: V3 {} Det1 {}".format(du[0], du[1]))
-print("dutch p-values: V3 {} Det1 {}".format(stats.ttest_rel(dg[2],du[2])[1], stats.ttest_rel(dg[3],du[3])[1]))
+def print_results(language, folder, indices):
+    grammatical = mean_surprisal(folder+language+'_gram_result.txt', indices)
+    ungrammatical = mean_surprisal(folder+language+'_ungram_result.txt', [i-1 for i in indices])
+    print(language+' results:')
+    print("grammatical:   V3 {} Det1 {}".format(grammatical[0], grammatical[1]))
+    print("ungrammatical: V3 {} Det1 {}".format(ungrammatical[0], ungrammatical[1]))
+    print("p-values:      V3 {} Det1 {}".format(stats.ttest_rel(list(grammatical[2].values()),list(ungrammatical[2].values()))[1],
+                                                stats.ttest_rel(list(grammatical[3].values()),list(ungrammatical[3].values()))[1]))
+    if args.plot:
+        plt.bar([i-.2 for i in range(len(grammatical[2]))],list(grammatical[2].values()), width=.4)
+        plt.bar([i+.2 for i in range(len(ungrammatical[2]))],list(ungrammatical[2].values()), width=.4)
+        plt.xticks([i for i in range(len(grammatical[2]))], list(grammatical[2].keys()))
+        plt.ylabel('surprisal')
+        plt.xlabel('sentence index')
+        plt.title('V3'+language)
+        plt.show()
+        plt.bar([i-.2 for i in range(len(grammatical[3]))],list(grammatical[3].values()), width=.4)
+        plt.bar([i+.2 for i in range(len(ungrammatical[3]))],list(ungrammatical[3].values()), width=.4)
+        plt.xticks([i for i in range(len(grammatical[3]))], list(grammatical[3].keys()))
+        plt.ylabel('surprisal')
+        plt.xlabel('sentence index')
+        plt.title('Det1'+language)
+        plt.show()
+
+folder = args.data_dir
+if args.language is None:
+    print_results('english', folder, list(english_indices.values()))
+    print('-'*65)
+    print_results('dutch', folder, list(dutch_indices.values()))
+else:
+    if args.language == 'english':
+        print_results(args.language, folder, list(english_indices.values()))
+    elif args.language == 'dutch':
+        print_results(args.language, folder, list(dutch_indices.values()))
